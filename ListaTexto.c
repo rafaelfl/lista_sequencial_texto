2e0c4817fceb92df9c8796acfe714d3da0ef5f26
Rafael Fernandes Lopes <31193433+rafaelfl@users.noreply.github.com>
2021-12-16 23:08:17 +0000

    Initial commit

---
+++ ListaTexto.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ListaTexto.h"
+
+ListaTexto * cria_lista() {}
+
+void destroi_lista(ListaTexto *l) {}
+
+// adiciona ao final da lista
+int adicionar_lista(ListaTexto *l, char *texto) {}
+
+// insere em alguma posição da lista
+int inserir_lista(ListaTexto *l, int posicao, char *texto) {}
+
+// retornar o elemento na posição 'posicao'
+char * acessar_lista(ListaTexto *l, int posicao) {}
+
+// retornar o índice do texto buscado ou -1 caso não encontre
+int buscar_lista(ListaTexto *l, char *buscado) {}
+
+// remove um elemento da lista e retorna ele ou NULL caso a posição não exista na lista
+char * remover_lista(ListaTexto *l, int posicao) {}
+
+void imprimir_lista(ListaTexto *l) {}
